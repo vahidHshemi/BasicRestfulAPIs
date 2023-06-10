9944b40b1f62dde85f822d49a0f4a067e5cfe17f
vahid hashemi <vahidhashemi73@gmail.com>
2023-06-10 18:06:46 +0330

    as second step we changed view functions based on api-view decorator of restframework

--- store/views.py
+++ store/views.py
@@ -1,21 +1,41 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from django.http import HttpResponse
 from .models import Product, Collection
 # Create your views here.
 
 # this is the basic view function based on the HttpRequest and HttpResponce of Django
+# def product_list(request):
+    
+#     return HttpResponse('you are in product list page')
+
+# def product_detail(request):
+    
+#     return HttpResponse('you are in product detail page')
+
+# def collection_list(request):
+    
+#     return HttpResponse('you are in collection list page')
+
+# def collection_detail(request):
+    
+#     return HttpResponse('you are in collection detail page')
+
+# as second try make view function based on api-view of resframework
+
+@api_view()
 def product_list(request):
-    
-    return HttpResponse('you are in product list page')
+    return Response("you are in product list page")
 
+@api_view()
 def product_detail(request):
-    
-    return HttpResponse('you are in product detail page')
+    return Response("you are in product detail page")
 
-def collection_list(request):
-    
-    return HttpResponse('you are in collection list page')
+@api_view()
+def collction_list(request):
+    return Response("you are in collection list page")
 
+@api_view()
 def collection_detail(request):
-    
-    return HttpResponse('you are in collection detail page')
+    return Response("you are in collection detail page")
