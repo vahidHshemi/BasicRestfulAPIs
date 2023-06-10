abedf4c9d8efbccacb0edf291e99d015f53dc27c
vahid hashemi <vahidhashemi73@gmail.com>
2023-06-10 23:11:12 +0330

    change unit_price to price then create a custome serializer field for price_with_tax

--- store/serializers.py
+++ store/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
+from decimal import Decimal
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=100)
     description = serializers.CharField(max_length=800)
-    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+    
+    # creating customize serializer fields
+    price_with_tax = serializers.SerializerMethodField(method_name='claculate_tax')
+    
+    def claculate_tax(self, unit_price):
+        return unit_price * Decimal(1.1)
 
 class CollectionSerializer(serializers.Serializer):
     id = serializers.IntegerField()
